afc676347983a52522d58a84a7c9f418b029b0f8
fightypants <kha.les.email@gmail.com>
2015-04-24 16:33:49 -0700

    it passes IT and UT tests

--- spec/word_counter_app_IT_spec.rb
+++ spec/word_counter_app_IT_spec.rb
@@ -1,6 +1,5 @@
-require ('./lib/word_counter_app')
-require ('sinatra')
 require ('capybara/rspec')
+require ('./app')
 
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
@@ -8,10 +7,9 @@ set(:show_exceptions, false)
   describe('the word counter path', {:type => :feature}) do
     it('processes the user input and returns word count for indentified word') do
       visit('/')
-      fill_in('sentence', {:with => 'happy happy joy joy hapy happy'})
-      fill_in('word'), {:with => 'happy'}
-      click_button('Submit')
-      expect(page).to(have_content('4'))
+      fill_in("sentence", :with => "happy happy joy joy happy")
+      fill_in("word", :with => "happy")
+      click_button("Count your word!")
+      expect(page).to(have_content('3'))
     end
   end
-end
--- spec/word_counter_app_UT_spec.rb
+++ spec/word_counter_app_UT_spec.rb
@@ -1,9 +1,11 @@
 require ('rspec')
-require ('./lib/word_counter')
+require ('./lib/word_counter_app')
+
 
 
 
 describe("String#word_counter") do
   it("Counts how many times we get a specifc word") do
-    expect("hello kha hello".coin_app()).to(eq(2))
+    expect("hello kha hello".word_count()).to(eq({"hello"=>2, "kha"=>1}))
+  end
  end
