72e28f3a5836c7cc7a5362f8450a39a448f80eed
fightypants <kha.les.email@gmail.com>
2015-04-24 16:02:52 -0700

    updated DIR structure

--- spec/word_counter_app_IT_spec.rb
+++ spec/word_counter_app_IT_spec.rb
@@ -8,7 +8,7 @@ set(:show_exceptions, false)
   describe('the word counter path', {:type => :feature}) do
     it('processes the user input and returns word count for indentified word') do
       visit('/')
-      fill_in('sentence'), {:with => 'happy happy joy joy hapy happy'})
+      fill_in('sentence', {:with => 'happy happy joy joy hapy happy'})
       fill_in('word'), {:with => 'happy'}
       click_button('Submit')
       expect(page).to(have_content('4'))
