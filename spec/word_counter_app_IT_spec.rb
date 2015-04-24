deec2faeeaef2f569d1b07eef9244bd08303bc4f
fightypants <kha.les.email@gmail.com>
2015-04-24 16:39:00 -0700

    update spec verbiege, create README

--- spec/word_counter_app_IT_spec.rb
+++ spec/word_counter_app_IT_spec.rb
@@ -5,7 +5,7 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
   describe('the word counter path', {:type => :feature}) do
-    it('processes the user input and returns word count for indentified word') do
+    it('processes the users inputs and returns word count for indentified word') do
       visit('/')
       fill_in("sentence", :with => "happy happy joy joy happy")
       fill_in("word", :with => "happy")
